6b8c81cf68c3ab981b8141c1e738215d1380bf9c
christina <chrstnaheadley@gmail.com>
2022-10-19 15:29:56 -0400

    testing

---
+++ 12-portals-and-refs/src/__tests__/useBreedList.test.js
@@ -0,0 +1,67 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { expect, test } from "@jest/globals";
+import { renderHook } from "@testing-library/react-hooks";
+import useBreedList from "../useBreedList";
+
+test("gives an empty list with no animals", async () => {
+  const { result } = renderHook(() => useBreedList("")); // or useBreedList()
+  const [breedList, status] = result.current;
+
+  expect(breedList).toHaveLength(0);
+  expect(status).toBe("unloaded");
+});
+
+test("gives back breeds with an animal", async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+
+  const { result, waitForNextUpdate } = renderHook(() => useBreedList("dog"));
+
+  await waitForNextUpdate();
+
+  const [breedList, status] = result.current;
+  expect(status).toBe("loaded");
+  expect(breedList).toEqual(breeds);
+});
+
+// HARD WAY W/O LIBRARY:
+
+// import { expect, test } from "@jest/globals";
+// import { render } from "@testing-library/react";
+// import useBreedList from "../useBreedList";
+
+// function getBreedList(animal) {
+//   let list;
+
+//   function TestComponent() {
+//     list = useBreedList(animal);
+//     return null;
+//   }
+
+//   render(<TestComponent />);
+
+//   return list;
+// }
+
+// test("gives an empty list with no animals", async () => {
+//   const [breedList, status] = getBreedList();
+//   expect(breedList).toHaveLength(0);
+//   expect(status).toBe("unloaded");
+// });
